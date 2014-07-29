ff6b71c01f2681e4d4c6a234ee3780beedc2eacd
Jan Sanchez <joejansanchez@gmail.com>
2014-07-29 15:51:09 -0500

    Gulp: Stylus - Agregando estilos

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	coffee = require('gulp-coffee'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+    stylus = require('gulp-stylus');
 
 gulp.task('default', ['jade', 'coffee'], function() {
 	console.log('aqui se deben ejecutar todas las tareas de gulp');
@@ -19,6 +20,12 @@ gulp.task('coffee', function() {
     gulp.src('./frontend/coffee/**/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./public/static/js/'))
+});
+
+gulp.task('stylus', function () {
+    gulp.src('./frontend/stylus/**/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./public/static/css/'));
 });
 
 /*
